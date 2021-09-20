6d2804c2fa687b6763a094436f5820a423ff5f2e
Robert Haas <rhaas@postgresql.org>
2021-09-20 13:40:15 -0400

    lock ordering rules

--- src/backend/access/conveyor/cbxlog.c
+++ src/backend/access/conveyor/cbxlog.c
@@ -35,6 +35,12 @@ cb_xlog_insert_payload_page(XLogReaderState *record)
 	Buffer		metabuffer;
 	Buffer		payloadbuffer;
 
+	if (!XLogRecHasBlockImage(record, 1))
+		elog(ERROR, "XLOG_CONVEYOR_INSERT_PAYLOAD_PAGE record did not contain full page image of payload block");
+	if (XLogReadBufferForRedo(record, 1, &payloadbuffer) != BLK_RESTORED)
+		elog(ERROR, "unexpected XLogReadBufferForRedo result when restoring backup block");
+
+	/* last due to lock ordering rules; see README */
 	if (XLogReadBufferForRedo(record, 0, &metabuffer) == BLK_NEEDS_REDO)
 	{
 		Page	metapage = BufferGetPage(metabuffer);
@@ -47,11 +53,6 @@ cb_xlog_insert_payload_page(XLogReaderState *record)
 		PageSetLSN(metapage, lsn);
 		MarkBufferDirty(metabuffer);
 	}
-
-	if (!XLogRecHasBlockImage(record, 1))
-		elog(ERROR, "XLOG_CONVEYOR_INSERT_PAYLOAD_PAGE record did not contain full page image of payload block");
-	if (XLogReadBufferForRedo(record, 1, &payloadbuffer) != BLK_RESTORED)
-		elog(ERROR, "unexpected XLogReadBufferForRedo result when restoring backup block");
 
 	UnlockReleaseBuffer(metabuffer);
 	UnlockReleaseBuffer(payloadbuffer);
@@ -71,6 +72,17 @@ cb_xlog_allocate_payload_segment(XLogReaderState *record)
 
 	xlrec = (xl_cb_allocate_payload_segment *) XLogRecGetData(record);
 
+	if (have_fsm_page &&
+		XLogReadBufferForRedo(record, 1, &fsmbuffer) == BLK_NEEDS_REDO)
+	{
+		Page	fsmpage = BufferGetPage(fsmbuffer);
+
+		cb_fsmpage_set_fsm_bit(fsmpage, xlrec->segno, true);
+		PageSetLSN(fsmpage, lsn);
+		MarkBufferDirty(fsmbuffer);
+	}
+
+	/* last due to lock ordering rules; see README */
 	if (XLogReadBufferForRedo(record, 0, &metabuffer) == BLK_NEEDS_REDO)
 	{
 		Page	metapage = BufferGetPage(metabuffer);
@@ -84,16 +96,6 @@ cb_xlog_allocate_payload_segment(XLogReaderState *record)
 			cb_metapage_set_fsm_bit(meta, xlrec->segno, true);
 		PageSetLSN(metapage, lsn);
 		MarkBufferDirty(metabuffer);
-	}
-
-	if (have_fsm_page &&
-		XLogReadBufferForRedo(record, 1, &fsmbuffer) == BLK_NEEDS_REDO)
-	{
-		Page	fsmpage = BufferGetPage(fsmbuffer);
-
-		cb_fsmpage_set_fsm_bit(fsmpage, xlrec->segno, true);
-		PageSetLSN(fsmpage, lsn);
-		MarkBufferDirty(fsmbuffer);
 	}
 
 	if (BufferIsValid(metabuffer))
@@ -122,21 +124,6 @@ cb_xlog_allocate_index_segment(XLogReaderState *record)
 
 	xlrec = (xl_cb_allocate_index_segment *) XLogRecGetData(record);
 
-	if (XLogReadBufferForRedo(record, 0, &metabuffer) == BLK_NEEDS_REDO)
-	{
-		Page	metapage = BufferGetPage(metabuffer);
-		CBMetapageData *meta;
-
-		meta = cb_metapage_get_special(metapage);
-		cb_metapage_add_index_segment(meta, xlrec->segno);
-		if (xlrec->is_extend)
-			cb_metapage_increment_next_segment(meta, xlrec->segno);
-		if (!have_fsm_page)
-			cb_metapage_set_fsm_bit(meta, xlrec->segno, true);
-		PageSetLSN(metapage, lsn);
-		MarkBufferDirty(metabuffer);
-	}
-
 	if (XLogReadBufferForRedo(record, 1, &indexbuffer) == BLK_NEEDS_REDO)
 	{
 		Page	indexpage = BufferGetPage(indexbuffer);
@@ -164,6 +151,22 @@ cb_xlog_allocate_index_segment(XLogReaderState *record)
 		cb_fsmpage_set_fsm_bit(fsmpage, xlrec->segno, true);
 		PageSetLSN(fsmpage, lsn);
 		MarkBufferDirty(fsmbuffer);
+	}
+
+	/* last due to lock ordering rules; see README */
+	if (XLogReadBufferForRedo(record, 0, &metabuffer) == BLK_NEEDS_REDO)
+	{
+		Page	metapage = BufferGetPage(metabuffer);
+		CBMetapageData *meta;
+
+		meta = cb_metapage_get_special(metapage);
+		cb_metapage_add_index_segment(meta, xlrec->segno);
+		if (xlrec->is_extend)
+			cb_metapage_increment_next_segment(meta, xlrec->segno);
+		if (!have_fsm_page)
+			cb_metapage_set_fsm_bit(meta, xlrec->segno, true);
+		PageSetLSN(metapage, lsn);
+		MarkBufferDirty(metabuffer);
 	}
 
 	if (BufferIsValid(metabuffer))
