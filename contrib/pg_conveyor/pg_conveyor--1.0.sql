32644a4cb54f72f73f71ae07350e841d3e9c5ead
Dilip Kumar <dilipkumar@localhost.localdomain>
2021-10-14 19:42:59 +0530

    conveyor_ext

---
+++ contrib/pg_conveyor/pg_conveyor--1.0.sql
@@ -0,0 +1,32 @@
+/* contrib/pg_conveyor/pg_conveyor--1.0.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_conveyor" to load this file. \quit
+
+-- Initialize the conveyor belt for the relation.
+CREATE FUNCTION pg_conveyor_init(relid OID, blocks_per_seg int)
+RETURNS void
+AS 'MODULE_PATHNAME', 'pg_conveyor_init'
+LANGUAGE C STRICT;
+
+/* Insert given data in the relation's conveyor belt. */
+CREATE FUNCTION pg_conveyor_insert(relid OID, data TEXT)
+RETURNS void
+AS 'MODULE_PATHNAME', 'pg_conveyor_insert'
+LANGUAGE C STRICT;
+
+/* Read relation's conveyor belt data. */
+CREATE FUNCTION pg_conveyor_read(relid OID, blockno bigint)
+RETURNS TEXT
+AS 'MODULE_PATHNAME', 'pg_conveyor_read'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION pg_conveyor_truncate(relid OID, blockno bigint)
+RETURNS void
+AS 'MODULE_PATHNAME', 'pg_conveyor_truncate'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION pg_conveyor_vacuum(relid OID)
+RETURNS void
+AS 'MODULE_PATHNAME', 'pg_conveyor_vacuum'
+LANGUAGE C STRICT;
